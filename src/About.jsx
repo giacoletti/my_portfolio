a11f0fef622787af446aef0469ababd0d532a765
giacoletti <giacoletti92@gmail.com>
2021-11-16 19:10:37 +0100

    Installing react-router-dom, creating About.jsx and Projects.jsx components setting up BrowserRouter in src/index.js

---
+++ src/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+const About = () => {
+  return (
+    <Container>
+      <h1 id="about-header">About Me</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Quia quos quaerat atque explicabo corporis commodi 
+        perferendis delectus! Cum error architecto, natus doloremque 
+        reprehenderit quisquam iste perspiciatis deleniti asperiores, 
+        sapiente porro?
+      </p>
+    </Container>
+  );
+};
+
+export default About;
---
+++ src/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'semantic-ui-react';
+
+const Projects = () => {
+  return (
+    <Container>
+      <h1 id="projects-header">My Projects</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Labore assumenda magnam sequi, laboriosam quibusdam nihil 
+        consequatur! Architecto quae animi eum hic quidem ipsam 
+        tempora amet, perspiciatis excepturi aperiam, numquam quod!
+      </p>
+    </Container>
+  );
+};
+
+export default Projects;
