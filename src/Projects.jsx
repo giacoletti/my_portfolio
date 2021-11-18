65716ef2e12c422858be84a251303561884442d7
giacoletti <giacoletti92@gmail.com>
2021-11-18 13:41:02 +0100

    Refactoring Projects.jsx, converting class component into functional component, using hooks

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,37 +1,34 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import { Container, Grid } from 'semantic-ui-react';
 import axios from 'axios';
 import ProjectCard from './ProjectCard';
 
-class Projects extends Component {
-  state = {
-    projects: []
-  };
-  
-  componentDidMount() {
-    axios.get("./data/projects.json").then((response) => {
-      this.setState({ projects: response.data });
-    });
-  };
+const Projects = () => {
 
-  render() {
-    const { projects } = this.state;
+  const [projects, setProjects] = useState([]);
 
-    let projectsList = projects.map((project) => {
-      return (
-        <div id={`project-${project.id}`} key={project.id}>
-          <ProjectCard project={project} />
-        </div>
-      );
-    });
-
+  let projectList = projects.map((project) => {
     return (
-      <Container>
-        <h1 id="projects-header">My Projects</h1>
-        <Grid>{projectsList}</Grid>
-      </Container>
-    );
-  };
+      <div id={`project-${project.id}`} key={project.id}>
+        <ProjectCard project={project} />
+      </div>
+    )
+  });
+
+  useEffect(() => {
+
+    axios.get("./data/projects.json").then((response) => {
+      setProjects(response.data);
+    });
+
+  }, []);
+
+  return (
+    <Container>
+      <h1 id="project-header">My Projects</h1>
+      <Grid>{projectList}</Grid>
+    </Container>
+  );
 
 };
 
