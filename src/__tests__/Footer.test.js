b83c931c45fe440f149e626cf9f4d89d6f0360ed
giacoletti <giacoletti92@gmail.com>
2021-11-18 15:19:58 +0100

    Creating component tests for Footer.jsx and ProjectCard.jsx component test Footer - is expected to display footer message component test ProjectCard - is expected to display title of project cleaning unnecessary icons data from manifest.json and index.html

---
+++ src/__tests__/Footer.test.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Footer from '../Footer';
+import { render, screen } from '@testing-library/react';
+
+describe('Footer.jsx', () => {
+  beforeEach(() => {
+    render(<Footer />);
+  });
+
+  it('is expected to display footer message', () => {
+    expect(
+      screen.getByText(`Made with React ${React.version}`)
+    ).toBeInTheDocument();
+  });
+});
---
+++ src/__tests__/ProjectCard.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ProjectCard from '../ProjectCard';
+import { render, screen } from '@testing-library/react';
+
+const project = {
+  "id": 1,
+  "name": "My Test Website",
+  "image": "https://images.unsplash.com/photo-1448375240586-882707db888b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+  "description": "This was my first project."
+};
+
+describe('ProjectCard.jsx', () => {
+  beforeEach(() => {
+    render(<ProjectCard project={project} />);
+  });
+
+  it('is expected to display the title of the project', () => {
+      expect(screen.getByText('My Test Website')).toBeInTheDocument();
+  });
+});
