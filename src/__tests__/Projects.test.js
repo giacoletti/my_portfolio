d17496f2524568524a8f8f967aaa174b9f935068
giacoletti <giacoletti92@gmail.com>
2021-11-19 12:30:53 +0100

    Adding component tests to Projects.jsx - expected to make a call using axios (WIP), expected to display the header

---
+++ src/__tests__/Projects.test.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Projects from '../Projects';
+import { render, screen, act } from '@testing-library/react';
+import axios from 'axios';
+
+describe('Projects.jsx', () => {
+  let axiosSpy;
+
+  const mockedResponse = [
+    {
+      "id": 1,
+      "name": "My First Website",
+      "image": "https://images.unsplash.com/photo-1448375240586-882707db888b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
+      "description": "This was my first project."
+    }
+  ];
+
+  beforeEach(() => {
+    // axiosSpy = jest.spyOn(axios, 'get').mockResolvedValue(mockedResponse);
+    act(() => {
+      render(<Projects />);
+    });
+  });
+
+  xit('is expected to make a call using axios', () => {
+    expect(axiosSpy).toHaveBeenCalled();
+  });
+
+  it('is expected to display the header', () => {
+    expect(screen.getByText('My Projects')).toBeInTheDocument();
+  });
+});
