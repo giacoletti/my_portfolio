9d65f5eecb8b3994dacd0bf231aa3d9e93f4329d
giacoletti <giacoletti92@gmail.com>
2021-11-20 18:55:52 +0100

    Adding component tests for JobCard.jsx - expected to display company name, job title, period and description creating JobCard.jsx

---
+++ src/JobCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from 'semantic-ui-react';
+
+const JobCard = ({ job }) => {
+  return (
+    <Card.Group>
+
+    </Card.Group>
+  );
+};
+
+export default JobCard;
--- src/__tests__/JobCard.test.js
+++ src/__tests__/JobCard.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import CVCard from '../CVCard';
+import JobCard from '../JobCard';
 import { screen, render } from '@testing-library/react';
 
 const job = {
@@ -10,3 +10,27 @@ const job = {
   "description": "Spotting excavators for precise excavation operations, laying pipes, steel fixing, operating power tools etc.",
   "link": "https://www.bmd.com.au/our-companies/bmd-constructions/"
 };
+
+describe('JobCard.jsx', () => {
+  beforeEach(() => {
+    render(<JobCard job={job} />);
+  });
+
+  it('is expected to display the company name', () => {
+    expect(screen.getByText('BMD Constructions')).toBeVisible();
+  });
+
+  it('is expected to display job title', () => {
+    expect(screen.getByText('Construction Labourer')).toBeVisible();
+  });
+
+  it('is expected to display the time period', () => {
+    expect(screen.getByText('May 2020 - March 2021')).toBeVisible();
+  });
+
+  it('is expected to display the description', () => {
+    expect(screen.getByText(
+      'Spotting excavators for precise excavation operations, laying pipes, steel fixing, operating power tools etc.'
+    )).toBeVisible();
+  });
+});
