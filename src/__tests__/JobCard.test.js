f708dda3be6d9dc47b7e75959a9df87b1a402487
giacoletti <giacoletti92@gmail.com>
2021-11-20 18:33:51 +0100

    Creating work_experience.json and education.json creating component test for JobCard.jsx (WIP)

---
+++ src/__tests__/JobCard.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CVCard from '../CVCard';
+import { screen, render } from '@testing-library/react';
+
+const job = {
+  "id": 1,
+  "company": "BMD Constructions",
+  "jobtitle": "Construction Labourer",
+  "period": "May 2020 - March 2021",
+  "description": "Spotting excavators for precise excavation operations, laying pipes, steel fixing, operating power tools etc.",
+  "link": "https://www.bmd.com.au/our-companies/bmd-constructions/"
+};
