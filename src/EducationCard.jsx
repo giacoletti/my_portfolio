20e423d0661a802e96d187cb2a5b6889d5477685
giacoletti <giacoletti92@gmail.com>
2021-11-21 12:42:41 +0100

    Creating EducationCard.jsx, relative component tests passing

---
+++ src/EducationCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card, Header } from 'semantic-ui-react';
+
+const EducationCard = ({ education }) => {
+  return (
+    <Card fluid color="yellow" className="educationCard">
+      <Card.Content>
+        <Header as="h2">{education.institute}</Header>
+        <Card.Header>{education.title}</Card.Header>
+        <Card.Meta>
+          <span className="educationDate">{education.period}</span>
+        </Card.Meta>
+        <Card.Description>{education.subjects}</Card.Description>
+      </Card.Content>
+    </Card>
+  );
+};
+
+export default EducationCard;
