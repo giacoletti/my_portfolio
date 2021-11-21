d8bb1d378de17d8b7d65519a1385a50548f8b4be
giacoletti <giacoletti92@gmail.com>
2021-11-21 20:48:35 +0100

    Adding component tests to Hello.jsx - expected to display first and second paragraph

--- src/__tests__/Hello.test.js
+++ src/__tests__/Hello.test.js
@@ -10,4 +10,12 @@ describe('Hello.jsx', () => {
   it('is expected to display a welcome message', () => {
     expect(screen.getByText('Welcome to my portfolio')).toBeVisible();
   });
+
+  it('is expected to display first paragraph', () => {
+    expect(screen.getByText('This website is a work in progress. More updates with more projects are on the way.')).toBeVisible();
+  });
+
+  it('is expected to display second paragraph', () => {
+    expect(screen.getByText('I made this website using the following technologies:')).toBeVisible();
+  });
 });
