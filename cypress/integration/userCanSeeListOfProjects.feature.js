a5282aaca0b3611320c58d846e36c91e431b1036
giacoletti <giacoletti92@gmail.com>
2022-03-04 23:36:24 +0100

    Add end2end test to check if visitor can see Recipe Hub project card with new icons and tooltips add GitHub icon and tooltip, add deployed icon and tooltip to ProjectCard, if links are not null update projects.json with Recipe Hub project (API and Client)

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -33,5 +33,39 @@ describe("User can see list of projects", () => {
       );
     });
   });
-  
+
+  describe("can see Recipe Hub client project", () => {
+    it("is expected to display relative project card", () => {
+      cy.get("#project-8").within(() => {
+        cy.get(".image").should("exist");
+        cy.get(".header").should("contain", "Recipe Hub (Client)");
+        cy.get(".description").should(
+          "contain.text",
+          "Social web application platform built to share recipes"
+        );
+      });
+    });
+
+    it("is expected to display GitHub icon link", () => {
+      cy.get("[data-cy=github-icon-8]").should("be.visible");
+    });
+
+    it("is expected to display GitHub tooltip", () => {
+      cy.get("[data-cy=github-icon-8]")
+        .trigger("mouseover")
+        .get("[data-cy=github-tooltip-8]")
+        .should("contain.text", "GitHub repo");
+    });
+
+    it("is expected to display deployed icon link", () => {
+      cy.get("[data-cy=deployed-icon-8]").should("be.visible");
+    });
+
+    it("is expected to display deployed icon tooltip", () => {
+      cy.get("[data-cy=deployed-icon-8]")
+        .trigger("mouseover")
+        .get("[data-cy=deployed-tooltip-8]")
+        .should("contain.text", "Deployed application");
+    });
+  });
 });
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,13 +1,50 @@
-import React from 'react';
-import { Card, Image } from 'semantic-ui-react';
+import React from "react";
+import { Card, Image, Icon, Popup } from "semantic-ui-react";
 
 const ProjectCard = ({ project }) => {
   return (
     <Card className="projectCard" href={project.url}>
-      <Image alt={`${project.name} image`} src={project.image} wrapped ui={false} />
+      <Image
+        alt={`${project.name} image`}
+        src={project.image}
+        wrapped
+        ui={false}
+      />
       <Card.Content>
         <Card.Header>{project.name}</Card.Header>
         <Card.Description>{project.description}</Card.Description>
+      </Card.Content>
+      <Card.Content extra>
+        {project.github && (
+          <a href={project.github}>
+            <Popup
+              data-cy={`github-tooltip-${project.id}`}
+              trigger={
+                <Icon
+                  data-cy={`github-icon-${project.id}`}
+                  name="github"
+                  size="large"
+                />
+              }
+              content="GitHub repo"
+            />
+          </a>
+        )}
+        {project.deployed && (
+          <a href={project.deployed}>
+            <Popup
+              data-cy={`deployed-tooltip-${project.id}`}
+              trigger={
+                <Icon
+                  data-cy={`deployed-icon-${project.id}`}
+                  name="globe"
+                  size="large"
+                />
+              }
+              content="Deployed application"
+            />
+          </a>
+        )}
       </Card.Content>
     </Card>
   );
